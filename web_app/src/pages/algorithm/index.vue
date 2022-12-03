0ca0ae24fe5b7c9c1a5eaf084efe20389123c69a
이윤건(Macko) <sangok1993@naver.com>
2022-12-03 19:11:56 +0900

    #15 markdown-viewer 생성
    예제를 가져오기 위한 store 기능도 추가

---
+++ web_app/src/pages/algorithm/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <algorithm-view />
+</template>
+
+<script>
+import AlgorithmView from "@/view/AlgorithmView";
+export default {
+  name: "Algorithm",
+  components: { AlgorithmView },
+};
+</script>
+
+<style scoped></style>
