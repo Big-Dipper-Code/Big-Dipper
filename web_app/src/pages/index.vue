208ad56b070fd6d27e8aa16e4bbaf570a9ff4d52
Harmon <dlawogur14@naver.com>
2023-02-09 19:55:03 +0900

    feat: 메인 페이지 정리 - button 형식

--- web_app/src/pages/index.vue
+++ web_app/src/pages/index.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png" />
-    <b-button variant="primary"> bootstrapVue3 Test</b-button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="d-flex justify-content-center">
+      <b-button class="mr-3" :to="Algorithm"> Algorithm </b-button>
+      <b-button> Block Test </b-button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
