a14bc1f65ed4159b61a2dcaadfe6e980fd1fb136
이윤건(Macko) <sangok1993@naver.com>
2023-02-09 21:43:02 +0900

    #32 간단한 라우팅 추가

--- web_app/src/pages/index.vue
+++ web_app/src/pages/index.vue
@@ -2,8 +2,10 @@
   <div class="home">
     <div class="text-center">Big Dipper</div>
     <div class="d-flex justify-content-center">
-      <b-button class="mr-3" :to="Algorithm"> Algorithm </b-button>
-      <b-button> Block Test </b-button>
+      <b-button class="mr-3" to="/algorithm"> Algorithm </b-button>
+      <b-dropdown text="Block Test">
+        <b-dropdown-item to="/tests/if-block">If Block</b-dropdown-item>
+      </b-dropdown>
     </div>
   </div>
 </template>
