d6243f9ec41300ec7b0edbb31dec222e6b08f0e3
Harmon <dlawogur14@naver.com>
2023-02-09 20:00:38 +0900

    feat: 메인페이지 정리 - 메인페이지의 불필요한 Vue 기본 요소들을 정리하였습니다.

--- web_app/src/pages/index.vue
+++ web_app/src/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <div class="text-center">Big Dipper</div>
     <div class="d-flex justify-content-center">
       <b-button class="mr-3" :to="Algorithm"> Algorithm </b-button>
       <b-button> Block Test </b-button>
